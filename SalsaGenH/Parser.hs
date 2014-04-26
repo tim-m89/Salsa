4eeba7cc01d2f55b466e552419a19de8691d8cec
Tim Matthews <tim.matthews7@gmail.com>
2014-04-26 14:58:34 +1200

    Lots of changes to the parser

--- SalsaGenH/Parser.hs
+++ SalsaGenH/Parser.hs
@@ -2,16 +2,37 @@
 
 module Parser where
 
+import Prelude hiding (takeWhile)
 import Control.Applicative
 import Data.Attoparsec.Text
-import Data.Attoparsec.Combinator
-import Data.Maybe
+import Data.Char (isDigit, isLetter)
+import Data.List (intercalate, find)
+import Data.Maybe (catMaybes)
 import qualified Data.Text as T
 
-data RequestMember = RequestMemberAll | RequestMemberText T.Text
+data RequestMember = RequestMemberAll | RequestMemberSpecific T.Text
 data ImportRequest = RequestRef T.Text | RequestType T.Text [RequestMember] 
-data ImportLine = ImportRequestLine ImportRequest | ImportComment | ImportBlank
-data ImportFile = ImportFile [ImportRequest]
+data ImportLine    = ImportRequestLine ImportRequest | ImportComment T.Text | ImportBlank
+data ImportFile    = ImportFile [ImportRequest]
+
+
+instance Show RequestMember where
+  show RequestMemberAll = "*"
+  show (RequestMemberSpecific t) = "<" ++ T.unpack t ++ ">"
+
+instance Show ImportRequest where
+  show (RequestRef t) = "reference <" ++ T.unpack t ++ ">"
+  show (RequestType t xs) = "type <" ++ T.unpack t ++ "> members: [" ++ intercalate  "," (map show xs) ++ "]"
+
+instance Show ImportLine where
+  show (ImportRequestLine x) = show x
+  show (ImportComment x) = "Comment --" ++ T.unpack x
+  show ImportBlank = "Blank"
+
+instance Show ImportFile where
+  show (ImportFile xs) = intercalate "\n" $ map show xs
+
+manySpace = many $ satisfy isHorizontalSpace
 
 parseMemAll :: Parser RequestMember
 parseMemAll = do
@@ -20,8 +41,8 @@ parseMemAll = do
 
 parseMemSpecific :: Parser RequestMember
 parseMemSpecific = do
-    t <- takeWhile1 (not . isHorizontalSpace)
-    return $ RequestMemberText t
+    t <- takeWhile1 (\c-> isDigit c || isLetter c)
+    return $ RequestMemberSpecific t
 
 parseMember :: Parser RequestMember
 parseMember = parseMemAll <|> parseMemSpecific
@@ -29,22 +50,33 @@ parseMember = parseMemAll <|> parseMemSpecific
 parseComment :: Parser ImportLine
 parseComment = do
     string "#" <|> string "--"
-    return ImportComment
+    t <- takeWhile (\c-> not $ isEndOfLine c)
+    return $ ImportComment t
 
 parseRef :: Parser ImportRequest
 parseRef = do
     string "reference"
     some space
     t <- takeTill isEndOfLine
-    many space
-    return $ RequestRef t
+    return $ RequestRef $ T.strip t
 
 parseType :: Parser ImportRequest
 parseType = do
-    t <- takeTill isHorizontalSpace
-    many space
-    mems <- sepBy1 parseMember (many space)
+    t <- takeWhile1 (\c-> isDigit c || isLetter c || c == '.')  
+    manySpace
+    option () $ skip $ (==) ':'
+    manySpace
+    mems <- sepBy parseMember (manySpace >> char ',' >> manySpace)
+    if length mems > 1 then do
+      case find matchMemAll mems of
+        (Just _) -> fail "Members should either be a comma seperated list or a single '*' but not both"
+        Nothing -> return ()
+    else return ()
+    manySpace
     return $ RequestType t mems
+    where matchMemAll :: RequestMember -> Bool
+          matchMemAll RequestMemberAll = True
+          matchMemAll _ = False
         
 parseImportRequest :: Parser ImportRequest
 parseImportRequest = parseRef <|> parseType
@@ -56,14 +88,14 @@ parseImportRequestLine = do
 
 parseBlank :: Parser ImportLine
 parseBlank = do
-    many space
-    endOfInput
+    skipWhile (\c-> not $ isEndOfLine c)
     return ImportBlank
 
 parseImportLine :: Parser ImportLine
 parseImportLine = do
-    many space
-    parseBlank <|> parseComment <|> parseImportRequestLine
+    manySpace
+    r <- parseComment <|> parseImportRequestLine <|> parseBlank
+    return r
 
 importLineAsImportRequest :: ImportLine -> Maybe ImportRequest
 importLineAsImportRequest l = case l of
